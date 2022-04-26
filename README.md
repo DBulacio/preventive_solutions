b6301ba7ed962975dbaa943960c96a132264a6fe
DBulacio <daniel.bulacio@hotmail.com>
2022-04-26 20:04:38 -0300

    First commit! Getting ready front!

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # preventive_solutions
 Landing page for Preventive Solutions Miami
+
+Objectives: 
+This should be a simple landing page with three sections.
+    - Corporate Presentation
+    - Service Description
+    - Contact (including wpp communication)
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="img/logo.ico" type="image/x-icon" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Welcome | Preventive Solutions Miami</title>
+</head>
+<body>
+    <header>
+        <img class="main-logo" src="img/logo.png" alt="Preventive Solutions Miami's Logo">
+        <h1 class="title text-center">We're getting ready!</h1>
+        <div class="ball"></div>
+    </header>
+    <p class="info text-center">1000 5th Street, Suite: 1316 <br>
+    Miami Beach, FL 33139 <br>
+    preventivesolutionsmiami@gmail.com <br>
+    +1 (786) 719-1538</p>
+    <button class="whatsapp-btn"><a href="https://api.whatsapp.com/send?phone=1(786)7191538&text=Hello!%20Happy%20to%20assist" target="_blank"><i class="fab fa-whatsapp" id="whatsapp-logo"></i></a></button>
+    <button class="instagram-btn"><a href="https://www.instagram.com/prev_solutions_miami/" target="_blank"><i class="fab fa-instagram" id="instagram-logo"></i></a></button>
+    
+</body>
+</html>
