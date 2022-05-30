968f568828ed7d0f0269641a39e2284efd415fd5
DBulacio <daniel.bulacio@hotmail.com>
2022-05-30 16:42:48 -0300

    more visual updates

--- footer.php
+++ footer.php
@@ -5,4 +5,8 @@
     +1 (786) 719-1538</p>
     <a href="https://github.com/DBulacio"><i class="fab fa-github"></i>
     <small> Diseñado por DBulacio &copy; 2022</small></a>
+    <a class="hidden" href="flaticon.com">SERVICE ICONS</a>
+    <a class="hidden" href="pexels.com">MAIN IMAGE</a>
+    <a class="hidden" href="freepik.com">ABOUT BACKGROUND</a>
+    <a class="hidden" href="fontawesome.com">OTHER ICONS</a>
 </footer>
